b59ccecdb42856885aaac7109f060b0cec9ac80f
Jose david Redondo <redondoperdomojosedavid@gmail.com>
2022-04-12 16:01:23 -0500

    Agregue el menu

--- src/functions.php
+++ src/functions.php
@@ -3,6 +3,12 @@ function init_template(){
 add_theme_support('post-thumbnails');
 add_theme_support('title-tag');
 
+register_nav_menus(
+    array(
+      'top-menu' => 'Menu principal'
+    )
+    );
+
 }
 
 add_action('after_setup_theme','init_template')
